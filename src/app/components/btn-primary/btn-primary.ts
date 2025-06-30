9254f6a210c157a6d044ab7bb6ec91f1c95f89bd
GeovannaLSouza <geovannamorila@gmail.com>
2025-06-30 19:33:24 -0300

    Completion of the first interactive button.

--- src/app/components/btn-primary/btn-primary.ts
+++ src/app/components/btn-primary/btn-primary.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { __importDefault } from 'tslib';
+
+type BtnVariats = "primary" | "secondary"
 
 @Component({
   selector: 'app-btn-primary',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './btn-primary.html',
   styleUrl: './btn-primary.scss'
 })
 export class BtnPrimary {
+  @__importDefault("btn-text") btnText: string = "";
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() variant: BtnVariats = "primary"
+  @Output("submit") onSubmit = new EventEmitter();
 
+  submit (){
+    this.onSubmit.emit
+  }
 }
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header";
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimary } from "../btn-primary/btn-primary";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimary],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
