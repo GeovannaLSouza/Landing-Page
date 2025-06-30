c6465db7bf3fe9696c179610415a6ebb8c380e2a
GeovannaLSouza <geovannamorila@gmail.com>
2025-06-30 20:49:26 -0300

    Configuring the fields for the user to enter their name and email for registration.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header";
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimary } from "../btn-primary/btn-primary";
+import { NewsletterForm } from "../newsletter-form/newsletter-form";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimary],
+  imports: [
+    HeaderComponent, 
+    NgOptimizedImage, 
+    BtnPrimary, 
+    NewsletterForm
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
