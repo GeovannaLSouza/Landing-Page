b446d8fb9b065765a4128d7b0d857e596f66e6ed
GeovannaLSouza <geovannamorila@gmail.com>
2025-06-30 21:38:29 -0300

    Applying the ReactiveFormsModule Application

--- src/app/components/newsletter-form/newsletter-form.ts
+++ src/app/components/newsletter-form/newsletter-form.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { BtnPrimary } from "../btn-primary/btn-primary";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
-  imports: [BtnPrimary],
+  imports: [BtnPrimary,
+    ReactiveFormsModule
+  ],
   templateUrl: './newsletter-form.html',
   styleUrl: './newsletter-form.scss'
 })
 export class NewsletterForm {
+    newsletterForm!: FormGroup;
 
+    constructor() {
+      this.newsletterForm = new FormGroup({
+        name: new FormControl('', Validators.required),
+      });
+    }
 }
